2d14b48b468c65d3db00e5cc6adf91e44590e387
Gurmeet Singh <74553130+gurmeet-S@users.noreply.github.com>
2021-02-26 12:34:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,3 +20,36 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
  function modelLoaded(){
      console.log('Model Loaded!');
  }
+ function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+   }
+  
+  function gotResult(error,results){
+      if(error){
+          console.error(error);
+      }else{
+          console.log(results);
+          document.getElementById("result_emotion_name").innerHTML = results[0].label;
+          document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+          prediction_1 = results[0].label;
+          prediction_2 = results[1].label;
+          speak();
+          if(results[0].label == "victory"){
+              document.getElementById("update_emoji").innerHTML = "&#9996;";
+          }
+          if(results[0].label == "amazing"){
+              document.getElementById("update_emoji").innerHTML = "&#128076;";
+          }
+          if(results[0].label == "all the best"){
+              document.getElementById("update_emoji").innerHTML = "&#128077;";
+          }
+      }
+  }
+  
+  function speak(){
+      var synth = window.speechSynthesis;
+      speak_data_1 = prediction_1;
+      var utterThis = new SpeechSynthesisUtterance(speak_data_1);
+      synth.speak(utterThis);
+  }
